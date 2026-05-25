d9f7e86e9e2eb62a000dd9f164a197b2be3071bb
lunarLucent <109795653+lunarLucent@users.noreply.github.com>
2026-05-25 01:38:01 +0100

    added most of the methods needed by PathData

--- src/main/java/net/zic/ascension/api/core/path/PathData.java
+++ src/main/java/net/zic/ascension/api/core/path/PathData.java
@@ -1,4 +1,68 @@
 package net.zic.ascension.api.core.path;
 
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.player.Player;
+import net.zic.ascension.api.core.OriginSource;
+import net.zic.ascension.api.core.technique.TechniqueData;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+//TODO decide if i want to pass source or entity wrapper
 public interface PathData {
+    //──Getters────────────────────────────────────────────────────────
+    int getMajorRealm();
+    int getMinorRealm();
+
+    int getMaxMinorRealm(int majorRealm);
+    int getMaxMajorRealm();
+
+    double getProgress();
+
+    boolean isCultivating();
+
+    boolean isBreakingThrough();
+
+    Identifier getCurrentTechnique();
+
+    TechniqueData getCurrentTechniqueData();
+
+    ArrayList<Identifier> getTechniqueHistory();
+
+    Collection<Identifier> getUniqueTechniques();
+
+    boolean hasCultivatedTechnique(Identifier technique);
+
+    Identifier getTechniqueForRealm(int majorRealm);
+
+    TechniqueData getTechniqueData(Identifier technique);
+
+    Collection<Integer> getCultivatedRealms(Identifier technique);
+
+    //──Setters────────────────────────────────────────────────────────
+
+    void setMajorRealm(int majorRealm);
+    void setMinorRealm(int minorRealm);
+
+    void setProgress(double progress);
+
+    void setCurrentTechnique(Identifier technique);
+
+    void setTechniqueData(Identifier technique,TechniqueData data);
+
+    //──Logic────────────────────────────────────────────────────────
+    void onMajorRealmUp(OriginSource source);
+    void onMajorRealmDown(OriginSource source);
+    void onMinorRealmUp(OriginSource source);
+    void onMinorRealmDown(OriginSource source);
+
+    //takes in a potential realm change, and breaks it down into individual steps
+    void handlerRealmChange(OriginSource source,int newMajorRealm,int newMinorRealm);
+
+    //caches the current state then simulates applying it
+    void simulateProgression(OriginSource source);
+    //──Save Data────────────────────────────────────────────────────────
+
+    //──Network────────────────────────────────────────────────────────
+
 }
